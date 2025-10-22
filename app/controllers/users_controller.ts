30e60ad3130e748abd368d32ce43f5e9ffc06c12
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-10-22 13:06:37 +0400

    zaur

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,9 +1,8 @@
 import User from '#models/user';
 import type { HttpContext } from '@adonisjs/core/http'
-import hash from '@adonisjs/core/services/hash';
 
 export default class UsersController {
-    async login ({auth,request,response}: HttpContext) {
+    async login ({request,response}: HttpContext) {
         const {email,password} = request.only(['email','password'])
         // const user  = await auth.use('api').getUserOrFail()
         const user  = await User.verifyCredentials(email,password)
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,7 +1,6 @@
-import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import {  column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import CustomBaseModel from './customBaseModel.js'
